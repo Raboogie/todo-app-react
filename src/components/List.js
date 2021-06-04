3cc98a84126c6156901aefef93ba1250ab58232c
RykuPrime <Dynasty7@outlook.com>
2021-06-03 21:48:09 -0400

    Added localStorage save

--- src/components/List.js
+++ src/components/List.js
@@ -9,7 +9,7 @@ const List = (props) => {
   return (
       <>
       <article>
-          <ul className="bg-gray-700 mx-5 rounded-lg mt-10">
+          <ul className="bg-gray-700 mx-5 rounded-lg mt-10 sm:max-w-xl sm:mx-auto">
               {props.items.map(({id, title}) => (
                   <ul className="flex items-center justify-between p-5 border-b border-gray-600">
                       <li key={id} className="todo-list text-white py-3 tracking-wider">{title}</li>
